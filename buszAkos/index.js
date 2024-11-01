183bced1ad989dfa19f1376cf563683103123a77
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2024-11-01 19:15:26 +0100

    Tudja geci hol hagytam abba

--- buszAkos/index.js
+++ buszAkos/index.js
@@ -1,3 +1,60 @@
+var szimbolumok = ["Rombusz", "Pick", "Treff", "Szív"];
+var ertekek = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function pakliKeszit()
+{
+	let pakli = new Array();
+
+	for(let i = 0; i < szimbolumok.length; i++)
+	{
+		for(let x = 0; x < ertekek.length; x++)
+		{
+			let card = {Ertek: ertekek[x], Szimbolum: szimbolumok[i]};
+			pakli.push(card);
+		}
+	}
+
+	return pakli;
+}
+
+function keveres(pakli)
+{
+	for (let i = 0; i < 1000; i++)
+	{
+		let hely1 = Math.floor((Math.random() * pakli.length));
+		let hely2 = Math.floor((Math.random() * pakli.length));
+		let tmp = pakli[hely1];
+
+		pakli[hely1] = pakli[hely2];
+		pakli[hely2] = tmp;
+	}
+  return pakli;
+}
+function lapokRajz(pakli)
+{
+
+		let kartya = document.createElement("div");
+		let ertek = document.createElement("div");
+		let szimbol = document.createElement("div");
+		kartya.className = "kartya";
+		ertek.className = "ertek";
+		szimbol.className = "szimbolum" + pakli[0].Szimbolum;
+		ertek.innerHTML = pakli[0].Ertek;
+    kartya.appendChild(ertek)
+    kartya.appendChild(szimbol)
+    document.body.appendChild(kartya)
+}
+let pakli = pakliKeszit()
+pakli = keveres(pakli)
+
+
+
+
+
+
+
+
+
 function kezdes() {
   document.body.innerHTML = '<form action="" method="get" id="menu"></form>';
   document.getElementById("menu").innerHTML = '<div id="jatekosok">';
@@ -51,12 +108,15 @@ function elsoJatekResz() {
   pirosGomb.setAttribute("id", "pirosGomb");
   pirosGomb.addEventListener("click", function () {
     szin = "piros";
+    document.getElementById("pakliKep").addEventListener("click",lapokRajz(pakli))
+    pakli.pop()
   });
   pirosGomb.textContent = "A kártya piros lesz";
   let feketeGomb = document.createElement("button");
   feketeGomb.setAttribute("id", "feketeGomb");
   feketeGomb.addEventListener("click", function () {
     szin = "fekete";
+
   });
   feketeGomb.textContent = "A kártya fekete lesz";
   menu.appendChild(feketeGomb);
