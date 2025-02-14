ec5402c954ae9121798f5602a7a57b0ddc94f0fe
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2025-02-14 14:16:44 +0100

    hatterfele

--- UNO/index.js
+++ UNO/index.js
@@ -7,24 +7,28 @@ function kartyaKeszit() {
     let szin = [0, 1, 2, 3];
     let szamok = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let blankKartya = document.createElement("div")
+    let kartyaHatter = document.createElement("div")
+    kartyaHatter.setAttribute("id", "kartyaHatter")
     blankKartya.setAttribute("id", "blankKartya")
 
-    let randomSzam = Math.floor(Math.random() * szamok.length + 1) + 1;
+    let randomSzam = Math.floor(Math.random() * szamok.length);
     let randomSzin = Math.floor(Math.random() * szin.length);
     if (randomSzin == 0) {
-        blankKartya.style.backgroundColor = "red";
+        blankKartya.style.backgroundColor = "#A52A2A";
        
     }
     else if (randomSzin == 1) {
-        blankKartya.style.backgroundColor = "green";
+        blankKartya.style.backgroundColor = "#228B22";
     }
     else if (randomSzin == 2) {
-        blankKartya.style.backgroundColor = "blue";
+        blankKartya.style.backgroundColor = "#0047AB";
     }
     else if (randomSzin == 3) {
-        blankKartya.style.backgroundColor = "yellow";
+        blankKartya.style.backgroundColor = "#FDDA0D";
     }
+    kartyaHatter.appendChild(blankKartya)
+    blankKartya.innerHTML = randomSzam;
     document.body.appendChild(blankKartya);
-    blankKartya.innerHTML(randomSzam)
+   
 }
 
