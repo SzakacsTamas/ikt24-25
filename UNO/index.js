6497c06d87aaa4cf15d62b22070eb78748e32b85
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2025-02-20 12:35:16 +0100

    hgjhhjuk

--- UNO/index.js
+++ UNO/index.js
@@ -1,3 +1,7 @@
+
+
+
+
 let rakodoPlatform = document.createElement("div")
 rakodoPlatform.setAttribute("id", "rakodoPlatform")
 
@@ -11,34 +15,24 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
+function jatekosMenu()
+{
+    for (let i = 0; i < jatekosokSzama.value; i++) {
 
 
-function kartyaKeszit() {
-    let szin = [0, 1, 2, 3];
-    let szamok = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let blankKartya = document.createElement("div")
-    let kartyaHatter = document.createElement("div")
-    kartyaHatter.setAttribute("id", "kartyaHatter")
-    blankKartya.setAttribute("id", "blankKartya")
+        let jatekosMenuDiv=document.createElement("div")
+        jatekosMenuDiv.setAttribute("id","jatekosMenuDiv") 
+        let szovegDoboz = document.createElement("input")
+        szovegDoboz.setAttribute("id", "szovegDoboz")
+        szovegDoboz.setAttribute("placeholder","Játékosnév")
+    
+        jatekosMenuDiv.appendChild(szovegDoboz)
+        document.body.append(jatekosMenuDiv)
+    
+      }
 
-    let randomSzam = Math.floor(Math.random() * szamok.length);
-    let randomSzin = Math.floor(Math.random() * szin.length);
-    if (randomSzin == 0) {
-        blankKartya.style.backgroundColor = "#A52A2A";
-       
-    }
-    else if (randomSzin == 1) {
-        blankKartya.style.backgroundColor = "#228B22";
-    }
-    else if (randomSzin == 2) {
-        blankKartya.style.backgroundColor = "#0047AB";
-    }
-    else if (randomSzin == 3) {
-        blankKartya.style.backgroundColor = "#FDDA0D";
-    }
-    kartyaHatter.appendChild(blankKartya)
-    blankKartya.innerHTML = randomSzam;
-    document.body.appendChild(blankKartya);
-   
+
+
 }
+
 
---
+++ UNO/minta/index.js
@@ -0,0 +1,46 @@
+
+let gomb = document.getElementById("hozzaAd")
+let keret = document.getElementById("keret")
+
+gomb.addEventListener("click", function () {
+    hozzaAdas=document.getElementById("hozzaAd")
+    hozzaAd.remove()
+    let szovegDoboz = document.createElement("input")
+    szovegDoboz.setAttribute("id", "szovegDoboz")
+    szovegDoboz.setAttribute("placeholder","Játékosnév")
+    
+    
+
+    let deleteGomb = document.createElement("button")
+    deleteGomb.setAttribute("id", "deleteGomb")
+    deleteGomb.textContent="-"
+
+    let kicsiKeret =document.createElement("div")
+    kicsiKeret.setAttribute("id","kicsiKeret")
+    kicsiKeret.appendChild(szovegDoboz)
+    kicsiKeret.appendChild(deleteGomb)
+    deleteGomb.addEventListener("click", function () {
+        kicsiKeret.remove()
+    })
+    keret.appendChild(kicsiKeret)
+    szovegDoboz.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            if(szovegDoboz.value==""){
+                alert("Nem lehet üres!")
+            }
+            else{
+                let szovegLabel = document.createElement("label")
+                szovegLabel.setAttribute("id","szovegLabel")
+            szovegLabel.textContent = szovegDoboz.value
+            szovegLabel.addEventListener("dblclick",function(){
+                kicsiKeret.replaceChild(szovegDoboz,szovegLabel)    
+            })
+            kicsiKeret.replaceChild(szovegLabel,szovegDoboz)
+            }
+            
+        }
+    
+    })
+
+
+})
